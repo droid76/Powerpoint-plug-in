e1420cdbf57c89510c119a9f8385624e4bfc95df
Siddhant Shenoy <siddhant8897@gmail.com>
2020-07-10 21:02:57 -0700

    Major bug fix for BarGraph

--- src/taskpane/components/Bargraph.js
+++ src/taskpane/components/Bargraph.js
@@ -6,7 +6,12 @@ import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 import NavDropdown from "react-bootstrap/NavDropdown"
 import {contents} from './BargraphScriptContents.js'
-
+import * as d3module from 'd3'
+import d3tip from 'd3-tip'
+const d3 = {
+  ...d3module,
+  tip: d3tip
+}
 
 class Bargraph extends React.Component{
     constructor(props) {
@@ -17,19 +22,114 @@ class Bargraph extends React.Component{
     }
     
     componentDidMount() {
-        const s = document.createElement('script'); 
-        s.type = 'text/javascript';
-        s.async = true;
-        s.innerHTML = contents;
-        console.log(s);
-        this.instance.appendChild(s);
+
+        // const s = document.createElement('script'); 
+        // s.type = 'text/javascript';
+        // s.async = true;
+        // s.innerHTML = contents;
+        // console.log(s);
+        // this.instance.appendChild(s);
     }
+
+    drawGraph() {
+//         var svgContainer = d3.select("body").append("svg")
+//                                     .attr("width", 200)
+//                                     .attr("height", 200)
+//                                     .append("circle")
+// .attr("cx", 30)
+// .attr("cy", 30)
+// .attr("r", 20);
+        var margin = {top: 40, right: 20, bottom: 30, left: 40};
+        var width = 960 - margin.left - margin.right;
+        var height = 500 - margin.top - margin.bottom;
+
+        var formatPercent = d3.format(".0%");
+
+        var x = d3.scaleBand()
+            .rangeRound([0, width], .1);
+
+        var y = d3.scaleLinear()
+            .range([height, 0]);
+
+        var xAxis = d3.axisBottom(x);
   
+
+        var yAxis = d3.axisLeft(y)
+            .tickFormat(formatPercent);
+
+        var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-10, 0])
+        .html(function(d) {
+            return "<strong>Frequency:</strong> <span style='color:red'>" + d.frequency + "</span><br/><br/>"
+                + "<strong>Earnings:</strong> <span style='color:red'>" + d.earnings + "</span><br/><br/>"
+                + "<strong>Name:</strong> <span style='color:red'>" + d.name + "</span>";
+        })
+
+        var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        svg.call(tip);
+
+        var data = d3.csvParse(`letter,frequency,earnings,name
+                A,.08167,18,"Microsoft"
+                B,.01492,256,"Google"
+                C,.02780,562,"Mathworks"
+                D,.04253,5665,"Hello"`);
+
+        // d3.csv("./data.csv").then(function(data) {
+  
+
+        //     console.log("data is");
+        //     console.log(data);
+        x.domain(data.map(function(d) { return d.letter; }));
+        y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+
+
+        
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Frequency");
+
+        svg.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d.letter); })
+            .attr("width", x.bandwidth())
+            .attr("y", function(d) { console.log("y is"); console.log(d); return y(d.frequency); })
+            .attr("height", function(d) { return height - y(d.frequency); })
+            .on('mouseover', tip.show)
+            .on('mouseout', tip.hide)
+
+
+        function type(d) {
+            d.frequency = +d.frequency;
+            return d;
+        }
+    }
+
     render(){
         return(
-            <div>
+            <div style={}>
                 <h1>Bargraph Page</h1>
-                <div ref={el => (this.instance = el)} />
+                <h1> Hi Akshay! </h1>
+                {this.drawGraph()}
             </div>
         );      
     }
